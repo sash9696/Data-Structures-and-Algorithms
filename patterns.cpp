9163a7ccfa343894467547988b92d6a08c1359e4
Sahil Chopra <sahilchopra838@gmail.com>
2024-02-21 21:19:57 +0530

    added pattern 7 create a triangle

--- patterns.cpp
+++ patterns.cpp
@@ -77,6 +77,40 @@ void print5(int n)
     }
 }
 
+void print6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space before stars
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+          for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     // test case;
@@ -93,7 +127,9 @@ int main()
         // print2Alt(n);
         // print3(n);
         // print4(n);
-        print5(n);
+        // print5(n);
+        // print6(n);
+        print7(n);
     }
 
     return 0;

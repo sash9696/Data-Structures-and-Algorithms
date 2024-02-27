69cac4d8ac0bdd29b49a13e67bbf9b6a759de0b9
Sahil Chopra <sahilchopra838@gmail.com>
2024-02-27 23:51:07 +0530

    Update maths.cpp

--- basicMaths/maths.cpp
+++ basicMaths/maths.cpp
@@ -41,29 +41,32 @@ using namespace std;
 //     return false;
 
 // }
-bool armstrongCheck(int x){
+bool armstrongCheck(int x)
+{
     int duplicate = x;
-    int sum=0;
-    while(x > 0){
+    int sum = 0;
+    while (x > 0)
+    {
         int lastDigit = x % 10;
         x = x / 10;
 
-        sum  = sum + lastDigit*lastDigit*lastDigit;
+        sum = sum + lastDigit * lastDigit * lastDigit;
     }
-      
-    if(sum == duplicate) return true;
-    return false;
 
+    if (sum == duplicate)
+        return true;
+    return false;
 }
-int main(){
+int main()
+{
     int x;
     cin >> x;
     cout << "Input num is " << x << endl;
 
     // extractDigits(x);
-//    int isPalindrome =  reverseNum(x);
-//    cout << isPalindrome;
-int isArmstrong =  armstrongCheck(x);
-   cout << isArmstrong;
+    //    int isPalindrome =  reverseNum(x);
+    //    cout << isPalindrome;
+    int isArmstrong = armstrongCheck(x);
+    cout << isArmstrong;
     return 0;
 }

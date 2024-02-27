8feef2bcd4dfa041f53fd32aa240b9c01a7fef5e
Sahil Chopra <sahilchopra838@gmail.com>
2024-02-27 23:31:51 +0530

    reverseNumber

--- basicMaths/maths.cpp
+++ basicMaths/maths.cpp
@@ -5,16 +5,23 @@
 using namespace std;
 
 void extractDigits(int x){
-
+    int count = 0;
+    int reverseNum = 0;
     while (x > 0){
 
         int lastDigit = x % 10;
 
         x = x / 10;
 
+        reverseNum = (reverseNum * 10) + lastDigit;
+
         cout << lastDigit << endl;
+        count++;
 
     }
+
+    cout << "the no of digits are: " <<count << endl;
+    cout << "the reverse of the number is: " <<reverseNum;
 
 }
 int main(){

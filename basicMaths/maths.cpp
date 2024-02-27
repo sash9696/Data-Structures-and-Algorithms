599e9e374fd0007f7d1f76e80fffc3521ab3242e
Sahil Chopra <sahilchopra838@gmail.com>
2024-02-27 23:41:17 +0530

    isPalindrome

--- basicMaths/maths.cpp
+++ basicMaths/maths.cpp
@@ -4,24 +4,41 @@
 
 using namespace std;
 
-void extractDigits(int x){
-    int count = 0;
-    int reverseNum = 0;
-    while (x > 0){
+// void extractDigits(int x){
+//     int count = 0;
+//     int reverseNum = 0;
+//     while (x > 0){
 
+//         int lastDigit = x % 10;
+
+//         x = x / 10;
+
+//         reverseNum = (reverseNum * 10) + lastDigit;
+
+//         cout << lastDigit << endl;
+//         count++;
+
+//     }
+
+//     cout << "the no of digits are: " <<count << endl;
+//     cout << "the reverse of the number is: " <<reverseNum;
+
+// }
+bool reverseNum(int x){
+    int duplicate = x;
+    int revNum=0;
+    while(x > 0){
         int lastDigit = x % 10;
-
         x = x / 10;
 
-        reverseNum = (reverseNum * 10) + lastDigit;
-
-        cout << lastDigit << endl;
-        count++;
-
+        revNum = (revNum * 10) + lastDigit;
     }
+        cout << "Input num is " << duplicate << endl;
 
-    cout << "the no of digits are: " <<count << endl;
-    cout << "the reverse of the number is: " <<reverseNum;
+    cout << "reverse of input  num is " << revNum << endl;
+
+    if(revNum == duplicate) return true;
+    return false;
 
 }
 int main(){
@@ -29,7 +46,9 @@ int main(){
     cin >> x;
     cout << "Input num is " << x << endl;
 
-    extractDigits(x);
+    // extractDigits(x);
+   int isPalindrome =  reverseNum(x);
+   cout << isPalindrome;
 
     return 0;
 }

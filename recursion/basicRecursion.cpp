d510a7c3cd0c467a6b9e3686b1bc686c16cb2c26
Sahil Chopra <sahilchopra838@gmail.com>
2024-03-01 14:24:22 +0530

    printNTo1

--- recursion/basicRecursion.cpp
+++ recursion/basicRecursion.cpp
@@ -2,31 +2,46 @@
 #include <iostream>
 using namespace std;
 
-//print name n times
+// print name n times
 
-void printName(int i, int n){
+void printName(int i, int n)
+{
 
-    //TC => O(n) => n+1 function calls
-    //SC => O(n) => stack space
+    // TC => O(n) => n+1 function calls
+    // SC => O(n) => stack space
 
-    if(i > n){
+    if (i > n)
+    {
         return;
     }
-     cout << "Raj" <<endl;
-     printName(i+1, n);
+    cout << "Raj" << endl;
+    printName(i + 1, n);
 }
 
-void print1ToN(int i , int n){
+void print1ToN(int i, int n)
+{
 
-    if(i > n){
+    if (i > n)
+    {
         return;
     }
-    cout<< i << endl;
-    print1ToN(i+1,n);
+    cout << i << endl;
+    print1ToN(i + 1, n);
 }
 
+void printNTo1(int i, int n)
+{
 
-int main(){
+    if (n < i)
+    {
+        return;
+    }
+    cout << n << endl;
+    printNTo1(i, n - 1);
+}
+
+int main()
+{
 
     int n;
 
@@ -35,6 +50,6 @@ int main(){
     cout << "The input num is : " << n << endl;
 
     // printName(1,n);
-        print1ToN(1,n);
-
+    // print1ToN(1,n);
+    printNTo1(1, n);
 }

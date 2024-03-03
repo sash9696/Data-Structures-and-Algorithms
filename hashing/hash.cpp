ecd1bc8eeb790581e495d55b941cd43116871f6b
Sahil Chopra <sahilchopra838@gmail.com>
2024-03-03 20:57:28 +0530

    basic hasing setup

--- hashing/hash.cpp
+++ hashing/hash.cpp
@@ -3,8 +3,38 @@ using namespace std;
 
 
 
+
+
 int main(){
 
+    int n ;
+    cin >> n;
 
+    int arr[n];
+
+    for (int i = 0; i< n; i++){
+        cin >> arr[i];
+    }
+
+    //pre compute 
+    int hash[13] = {0};
+
+    for (int i = 0; i < n; i++){
+        hash[arr[i]] += 1;
+    }
+
+    // for(int x : hash){
+    //     cout << x;
+    // }
+
+    int q;
+    cin >>q ;
+
+    while(q --){
+        int number;
+        cin >> number;
+        //fetch
+        cout << hash[number] << endl;
+    }
     return 0;
 }

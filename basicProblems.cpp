c401631706a83cbed15bd8b8e1c7e77dba4c326d
Sahil Chopra <sahilchopra838@gmail.com>
2024-03-06 22:39:36 +0530

    Update basicProblems.cpp

--- basicProblems.cpp
+++ basicProblems.cpp
@@ -15,3 +15,18 @@ int main() {
     cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num = 5;
+    cout << "Factorial of " << num << " is: " << factorial(num) << endl;
+    return 0;
+}

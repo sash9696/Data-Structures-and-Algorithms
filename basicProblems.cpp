5a3189c9b98c857ead96dfe6767f39e51df6ebc5
Sahil Chopra <sahilchopra838@gmail.com>
2024-03-06 22:39:24 +0530

    Update basicProblems.cpp

--- basicProblems.cpp
+++ basicProblems.cpp
@@ -5,3 +5,13 @@ int main() {
     cout << "Hello, World!" << endl;
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 7;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}

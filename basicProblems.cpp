fee87b6a29247a006cbcec2d1addae3fd135a826
Sahil Chopra <sahilchopra838@gmail.com>
2024-03-06 22:40:24 +0530

    Update basicProblems.cpp

--- basicProblems.cpp
+++ basicProblems.cpp
@@ -69,3 +69,21 @@ int main() {
     cout << "Average of numbers in the array: " << calculateAverage(arr, size) << endl;
     return 0;
 }
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverseString(string str) {
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i], str[n - i - 1]);
+    }
+    return str;
+}
+
+int main() {
+    string str = "Hello, World!";
+    cout << "Original string: " << str << endl;
+    cout << "Reversed string: " << reverseString(str) << endl;
+    return 0;
+}

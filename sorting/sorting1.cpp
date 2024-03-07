e2e49ddc0203727345ed01030052fc18be0f21a9
Sahil Chopra <sahilchopra838@gmail.com>
2024-03-07 15:00:18 +0530

    insertionSort

--- sorting/sorting1.cpp
+++ sorting/sorting1.cpp
@@ -78,6 +78,19 @@ void bubbleSort(int arr[],int n){
 
 }
 
+void insertionSort(int arr[], int n){
+
+    for(int i = 0; i < n - 1; i++){
+
+        int j = i;
+
+        while (j >0 && arr[j-1] > arr[j]){
+            swap(arr, j - 1, j);
+            j -- ;
+        }
+    }
+}
+
 int main()
 {
     int n;
@@ -94,7 +107,9 @@ int main()
     // algo
 
     // selectionSort(arr, n);
-    bubbleSort(arr, n);
+    // bubbleSort(arr, n);
+    insertionSort(arr, n);
+
 
     for (int x : arr)
     {
